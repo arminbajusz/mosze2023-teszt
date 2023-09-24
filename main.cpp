377e725ea2526923dc232d483ca3f2f4c3897739
Bajusz_Armin <bajuszarmin60@gmail.com>
2023-09-24 17:23:43 +0200

    Hibák javítása

--- main.cpp
+++ main.cpp
@@ -4,23 +4,25 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //elírás
-    std::cout << '1-100 ertekek duplazasa' //aposztróf idézőjel helyett, pontosvessző hiányzik
-    for (int i = 0;)	//hiányos ciklus
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa:\n";
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        b[i] = i * 2; //Ez a 0-99 értékeket duplázza meg
+        b[i] = i * 2 + 2;
     }
-    for (int i = 0; i; i++) //hiányos ciklus
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" //Nem írja ki a kiszámolt értéket, pontosvessző hiányzik
+        std::cout << "Ertek: " << b[i] << std::endl;
     }    
-    std::cout << "Atlag szamitasa: " << std::endl; // vessző pontosvessző helyett
+    std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i] //pontosvessző hiányzik
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    return 0; //Lefoglalt memóriaterület nincs felszabadítva
+    delete[] b;
+    return 0;
 }
+
